9a82a027bb4af18092f413a5a36bdcca6f1d7134
Douglas 'Amarelo' Lopes <d.lopes.nascimento@gmail.com>
2018-03-10 09:12:18 -0300

    challenge 27

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -16,11 +16,11 @@
 		Array.prototype.map.call(this.element, function( element ) {
 			element.removeEventListener( listenerEvent, callbackFunction );
 		});
-	}
+	};
 
 	DOM.prototype.get = function get() {
 		return this.element;
-	}
+	};
 
 	/*
 	Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
@@ -41,20 +41,67 @@
 	O método isNull deve retornar `true` se o valor for null ou undefined.
 	*/
 
-	DOM.prototype.DOMForEach = function( element, callbackFunction ) {
-		Array.prototype.forEach.call( element, callbackFunction );
+	DOM.prototype.forEach = function forEach() {
+
+		console.log(this);
+		return Array.prototype.forEach.apply( this.element, arguments );
+	};
+
+	DOM.prototype.map = function map() {
+		return Array.prototype.map.apply( this.element, arguments );
+	};
+
+	DOM.prototype.filter = function filter() {
+		return Array.prototype.filter.apply( this.element, arguments );
+	};
+
+	DOM.prototype.reduce = function reduce() {
+		return Array.prototype.reduce.apply( this.element, arguments );
+	};
+
+	DOM.prototype.reduceRight = function reduceRight() {
+		return Array.prototype.reduceRight.apply( this.element, arguments );
+	};
+
+	DOM.prototype.every = function every() {
+		return Array.prototype.every.apply( this.element, arguments );
+	};
+
+	DOM.prototype.some = function some() {
+		return Array.prototype.some.apply( this.element, arguments );
+	};
+
+
+	DOM.prototype.isArray = function isArray( param ) {
+		return returnParam( param ) === '[object Array]';
 	}
 
-	var teste = new DOM();
-	console.log( teste.DOMForEach([1, 2, 3, 4, 5], function( item, index ) {
-		console.log( 'item ' + index, ( item * item ) );
-	}) );
-
-	DOM.isArray = function isArray() {
-		return Object.prototype.toString() === [Object Array] ? true : false;
+	DOM.prototype.isObject = function isObject( param ) {
+		return returnParam( param ) === '[object Object]';
 	}
 
-	console.log( DOM.isArray( [1,2,3,4,5] ) );
+	DOM.prototype.isFunction = function isFunction( param ) {
+		return returnParam( param ) === '[object Function]';
+	}
 
+	DOM.prototype.isNumber = function isNumber( param ) {
+		return returnParam( param ) === '[object Number]';
+	}
+
+	DOM.prototype.isString = function isString( param ) {
+		return returnParam( param ) === '[object String]';
+	}
+
+	DOM.prototype.isBoolean = function isBoolean( param ) {
+		return returnParam( param ) === '[object Boolean]';
+	}
+
+	DOM.prototype.isNull = function isNull( param ) {
+		return returnParam( param ) === '[object Null]' || returnParam( param ) === '[object Undefined]';
+	}
+
+	function returnParam( param ) {
+		return Object.prototype.toString.call( param );
+	}
 
 })( window, document );
